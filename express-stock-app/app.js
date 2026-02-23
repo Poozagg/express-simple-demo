2cd92382507665e9215d8e4227a573c55e43b5c4
Pooja Gurung <147177106+Poozagg@users.noreply.github.com>
2026-02-23 12:17:40 +0000

    chore: add nodemon for development auto-restart

--- express-stock-app/app.js
+++ express-stock-app/app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//routes for my express app
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/aboutus', (req, res) => {
+  res.send('About Us');
+});
+
+app.get('/contactus', (req, res) => {
+  res.send('Contact Us');
+}); 
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
